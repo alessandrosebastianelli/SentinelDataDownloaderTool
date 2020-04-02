2fbe2688278b80396e479dfa02c0ad69fd248556
Alessandro Sebastianelli <alessandroseba1995@gmail.com>
2020-04-02 18:21:19 +0200

    jsonHandlar to OO

--- gui_code/jsonHandler.py
+++ gui_code/jsonHandler.py
@@ -1,12 +1,13 @@
 import json
 
 
-with open("gui_code/settings.json", "r") as read_file:
-  data = json.load(read_file)
+class jsonHandeler:
 
-
-def get_component_settings(component_name):
-  settings = data[component_name]
-  return settings[0]
-
-print(get_component_settings('downloader')['number_of_scene'])
+  def __init__(self):
+    self.settings_file_path = "gui_code/settings.json"
+    with open(self.settings_file_path, "r") as read_file:
+      self.json_data = json.load(read_file)
+  
+  def get_component_settings(component_name):
+    settings = self.json_data[component_name]
+    return settings[0]
